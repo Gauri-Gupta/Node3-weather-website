559a5d9a201b2b2016573c8250e4497870d6549d
tech reformers <gaurigupta.181098@gmail.com>
2023-07-12 00:10:41 -0300

    Setup app for Render

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 //define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -140,6 +142,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, ()=>{
-    console.log('server is up and running on port 3000')
+app.listen(port, ()=>{
+    console.log('server is up and running on port ' + port )
 })
